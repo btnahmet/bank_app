4b8a318883d0c0694967b2038c264752d14e3285
btnahmet <ahmetbutun27@gmail.com>
2025-06-26 14:59:46 +0300

    Ana Sayfa Düzenleme Commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final width = size.width;
     final height = size.height;
+    final textScale = MediaQuery.of(context).textScaleFactor;
 
     return SafeArea(
       child: Scaffold(
@@ -16,24 +17,26 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: AppColors.primaryRed,
           foregroundColor: Colors.white,
-          title: const Text('Ana Sayfa'),
+          title: Text(
+            'Ana Sayfa',
+            style: TextStyle(fontSize: width * 0.05),
+          ),
           actions: [
             Padding(
-              padding: const EdgeInsets.only(right: 12.0),
+              padding: EdgeInsets.only(right: width * 0.03),
               child: GestureDetector(
                 onTap: () {
-                  // Profil sayfasına git veya alert göster vs.
-                  // Navigator.push(context, MaterialPageRoute(builder: (_) => const ProfileScreen()));
+                  // Profil sayfasına yönlendirme
                 },
                 child: CircleAvatar(
-                  radius: 18,
+                  radius: width * 0.045,
                   backgroundColor: Colors.white,
                   child: ClipOval(
                     child: Image.asset(
-                      'assets/images/profile_icon.png', // kendi resmini buraya koy
+                      'assets/images/profile_icon.png',
                       fit: BoxFit.cover,
-                      width: 32,
-                      height: 32,
+                      width: width * 0.08,
+                      height: width * 0.08,
                     ),
                   ),
                 ),
@@ -43,55 +46,117 @@ class HomeScreen extends StatelessWidget {
         ),
         body: Stack(
           children: [
-            // 👇 Scrollable içerik
-            SingleChildScrollView(
-              padding: EdgeInsets.only(bottom: height * 0.1),
-              child: Center(
-                child: Container(
-                  width: width * 0.85,
-                  margin: const EdgeInsets.only(top: 40),
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: AppColors.lightRed,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.05),
-                        blurRadius: 10,
-                        offset: const Offset(0, 4),
+            Center(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.only(bottom: height * 0.1),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: width,
+                      padding: EdgeInsets.all(width * 0.06),
+                      color: Colors.transparent,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  "Hesaplarım",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: width * 0.067,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  "Kredi Kartlarım",
+                                  textAlign: TextAlign.right,
+                                  style: TextStyle(
+                                    fontSize: width * 0.063,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: height * 0.02),
+
+                          Text(
+                            "Of/Trabzon",
+                            style: TextStyle(
+                              fontSize: width * 0.06,
+                              fontWeight: FontWeight.w500,
+                              color: AppColors.primaryRed,
+                            ),
+                          ),
+                          SizedBox(height: height * 0.01),
+                          // Text(
+                          //   "Vadesiz TL",
+                          //   style: TextStyle(
+
+                          //     fontSize: width * 0.045,
+                          //     fontWeight: FontWeight.w500,
+                          //   ),
+                          // ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: width * 0.03,
+                                vertical: height * 0.005),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(
+                                  255, 173, 127, 42), // arka plan rengi
+                              borderRadius: BorderRadius.circular(
+                                  width * 0.02), // köşe yumuşatma
+                            ),
+                            child: Text(
+                              "Vadesiz TL",
+                              style: TextStyle(
+                                fontSize: width * 0.045,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+
+                          SizedBox(height: height * 0.01),
+                          Text(
+                            "TR12 0001 0000 1234 5678 9100 00",
+                            style: TextStyle(
+                              fontSize: width * 0.0531,
+                            ),
+                          ),
+                          SizedBox(height: height * 0.025),
+                          Text(
+                            "Bakiye",
+                            style: TextStyle(
+                              fontSize: width * 0.042,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: height * 0.005),
+                          Text(
+                            "₺ 12.500,75",
+                            style: TextStyle(
+                              fontSize: width * 0.06,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Icon(Icons.account_balance_wallet,
-                          size: height * 0.08, color: AppColors.primaryRed),
-                      const SizedBox(height: 16),
-                      Text(
-                        "Hoş Geldiniz!",
-                        style: TextStyle(
-                          fontSize: width * 0.06,
-                          fontWeight: FontWeight.bold,
-                          color: AppColors.primaryRed,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        "Bakiye ve işlemlerinizi buradan takip edebilirsiniz.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: width * 0.04,
-                          color: AppColors.textColor,
-                        ),
-                      ),
-                      const SizedBox(height: 400), // örnek içerik
-                    ],
-                  ),
+                    ),
+                    SizedBox(height: height * 0.05),
+                  ],
                 ),
               ),
             ),
-
-            // 👇 Alt sabit bar
             Align(
               alignment: Alignment.bottomCenter,
               child: Container(
@@ -99,7 +164,7 @@ class HomeScreen extends StatelessWidget {
                 decoration: BoxDecoration(
                   color: AppColors.primaryRed,
                   borderRadius:
-                      const BorderRadius.vertical(top: Radius.circular(16)),
+                      BorderRadius.vertical(top: Radius.circular(width * 0.04)),
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -138,17 +203,18 @@ class _BottomBarItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Expanded(
       child: InkWell(
         onTap: onTap,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(icon, color: Colors.white),
-            const SizedBox(height: 4),
+            Icon(icon, color: Colors.white, size: width * 0.07),
+            SizedBox(height: width * 0.01),
             Text(
               label,
-              style: const TextStyle(color: Colors.white, fontSize: 12),
+              style: TextStyle(color: Colors.white, fontSize: width * 0.03),
             ),
           ],
         ),

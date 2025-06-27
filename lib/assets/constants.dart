584b70a6f04ede97a67d3f84a8f2840cf5567e57
btnahmet <ahmetbutun27@gmail.com>
2025-06-27 22:27:35 +0300

    Yorum Satırı Düzenleme Commit

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryRed = Color(0xFFd32f2f); // Ziraat kırmızısı
+  static const Color primaryRed = Color(0xFFd32f2f); // Kırmızı Renk
   static const Color lightRed = Color(0xFFFFEBEE);
   static const Color background = Colors.white;
   static const Color textColor = Colors.black87;
